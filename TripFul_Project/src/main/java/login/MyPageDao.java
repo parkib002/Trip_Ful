0883322c1810f2265ac57b8c7d44c64ed861d312
ChanHyeokKong <kch6355@naver.com>
2025-06-04 17:46:42 +0900

    0604 commit

--- TripFul_Project/src/main/java/login/MyPageDao.java
+++ TripFul_Project/src/main/java/login/MyPageDao.java
@@ -1,9 +1,25 @@
 package login;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import mysql.db.DbConnect;
 
 public class MyPageDao {
 	DbConnect db = new DbConnect();
 	
-	
+	public List<String> getWishList(String id){
+		List<String> wishList = new ArrayList<String>();
+		
+		Connection conn = db.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "select place_num from tripful_place_like where id=?";
+		
+		return wishList;
+	}
 }
