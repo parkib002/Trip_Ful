b03a864a77105966e564b5daf414118334a2cfc8
jungmin2803 <wjdals2803@naver.com>
2025-06-04 09:11:01 +0900

    review

--- TripFul_Project/src/main/webapp/Review/JavaScript/reviewListJs.js
+++ TripFul_Project/src/main/webapp/Review/JavaScript/reviewListJs.js
@@ -49,7 +49,6 @@ $(document).on("click", ".delete-btn", function(){
 });
 
 
-<<<<<<< HEAD:TripFul_Project/src/main/webapp/Review/JavaScript/reviewJs.js
 // 별점 클래스명을 반환하는 헬퍼 함수
 function getStarClass(star) {
     switch (star) {
@@ -61,26 +60,4 @@ function getStarClass(star) {
         default: return '';
     }
 }
-=======
 
-
-	$(document).on('click', '.category', function(e) {
-        // 현재 클릭된 아이콘의 review-id를 가져옵니다. (옵션)
-        var clickedId = $(this).attr("review_id");
-		console.log(clickedId);
-        // 다른 모든 열려있는 드롭다운 메뉴를 닫습니다.
-        // 현재 클릭된 메뉴만 열리도록 하기 위함.
-        $('.dropdown-menu').not($(this).next('.dropdown-menu')).slideUp(100);
-
-        // 현재 클릭된 아이콘 바로 다음에 오는 '.dropdown-menu'를 찾아서 토글합니다.
-        $(this).next('.dropdown-menu').slideToggle(100); // slideToggle로 부드러운 효과
-
-        // 클릭 이벤트가 버블링되는 것을 막아, document 클릭 시 닫히는 이벤트와 충돌 방지
-        e.stopPropagation();
-    });
-
-    // 2. 드롭다운 메뉴 외부를 클릭했을 때 메뉴 닫기
-    $(document).on('click', function() {
-        $('.dropdown-menu').slideUp(100);
-    });
->>>>>>> 838cfa62f91cf7f956f0a8316817e4751fe43878:TripFul_Project/src/main/webapp/Review/JavaScript/reviewListJs.js
